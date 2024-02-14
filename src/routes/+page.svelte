b52c3b8a24ba330222b3e5d4a5c1929fe7496cc3
DanilSergievskij <danilsergievskij95@gmail.com>
2024-02-14 12:31:58 +0300

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+   	/** @type {import('./$types').PageData} */
+	export let data;
+</script>
+
+<h1>List from dunny</h1>
+
+<p>Description list <a href = https://dummyjson.com/docs/products>https://dummyjson.com/docs/products</a></p>
+
+<div><img src = "{data.products.thumbnail}" alt="thumbnailFromList"> </div> 
+<div>ID{data.products.id}</div>
+<div>TITLE{data.products.title}</div>
+<div>Description {data.products.description}</div>
+<div>price {data.products.price} $</div>
+
+{#each data.products.images as imageSrc}
+     <div><img src={imageSrc} alt=productImage/></div>
+	 {/each}
