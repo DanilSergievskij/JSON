ba1bb944723b4568f201c97ac833d4f56c7c24c7
DanilSergievskij <danilsergievskij95@gmail.com>
2024-02-21 13:26:00 +0300

    Commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,49 +1,54 @@
 <script>
-// import { TinySlider } from "svelte-tiny-slider"
+
+import { enhance } from '$app/forms';
 	export let data;
+ let searchData=''
+
+ const searchFunc = () => {
+	console.log ('searchFiled', searchData)
+	fetch(`https://dummyjson.com/products/search?q=${searchData}`)
+			.then((res) => res.json())
+			.then((resData) => {
+				data.products = resData.products
+			});
+ }
+	
 </script>
 
 <h1>List from dunny</h1>
 
 <p>Description list <a href = https://dummyjson.com/docs/products>https://dummyjson.com/docs/products</a></p>
 
+<div class="join">
+	<input class="input input-bordered join-item" 
+	bind:value={searchData}
+	placeholder="Search"/>
+	<button class="btn join-item rounded-r-full" on:click={searchFunc}>Search</button>
+	
+  </div>
 
-{ #each data.products as product}
+
+<!-- { #each data.products as product}
 <div><img src = "{product.thumbnail}" alt="thumbnailFromList"> </div> 
 <div>ID{product.id}</div>
 <div>TITLE{product.title}</div>
 <div>Description {product.description}</div>
 <div>price {product.price} $</div>
-{/each}
+{/each} -->
 
 
 	
-	 <h1 class="text-3xl font-bold underline">
+	 <!-- <h1 class="text-3xl font-bold underline">
 		Hello world!
 	  </h1>
 
-	  <button class="btn">Button</button>
+	  <button class="btn">Button</button> -->
 
 
-<!-- 
-	  <TinySlider>
-		{#each data.products.images as imageSrc}
-		<img src="{imageSrc}" class="rounded-box" />
-		{/each}
-	  
-		<div slot="controls" let:setIndex let:currentIndex>
-		  {#each data.products.images as imageSrc}
-			<button
-			  class:active={i == currentIndex}
-			  on:click={() => setIndex(i)}
-			  on:focus={() => setIndex(i)}>
-			  <img src={imageSrc} alt="" height=60 />
-			</button>
-		  {/each}
-		</div>
-	  </TinySlider> -->
 
-	  <!-- <div class="overflow-x-auto">
+	
+
+	  <div class="overflow-x-auto">
 		<table class="table">
 
 		  <thead>
@@ -53,16 +58,15 @@
 				  <input type="checkbox" class="checkbox" />
 				</label>
 			  </th>
-			  <th>Name</th>
-			  <th>Job</th>
-			  <th>Favorite Color</th>
+			  <th>Title</th>
+			  <th>Description</th>
+			  <th>Ratingr</th>
 			  <th></th>
 			</tr>
 		  </thead>
 		  <tbody>
 		
-			{#each data.products.images as imageSrc}
-			<p>{imageSrc}</p>
+			{#each data.products as product}
 			<tr>
 				<th>
 				  <label>
@@ -73,56 +77,36 @@
 				  <div class="flex items-center gap-3">
 					<div class="avatar">
 					  <div class="mask mask-squircle w-12 h-12">
-						<img src={imageSrc} alt="Avatar Tailwind CSS Component" />
+						<img src={product.thumbnail} alt="Avatar Tailwind CSS Component" />
 					  </div>
 					</div>
 					<div>
-					  <div class="font-bold">Hart Hagerty</div>
-					  <div class="text-sm opacity-50">United States</div>
+					  <div class="font-bold">{product.title}</div>
+					  <div class="text-sm opacity-50"></div>
+					  <div class="text-sm opacity-50" style="text-decoration:line-through;color:red">{(product.price / 100) * product.discountPercentage + product.price} $</div>
+					  <div class="text-sm opacity-50">{product.price} $</div>
+					  <div class="text-sm opacity-50">{product.discountPercentage} %</div>
 					</div>
 				  </div>
 				</td>
 				<td>
-				  Zemlak, Daniel and Leannon
+				  description
 				  <br/>
-				  <span class="badge badge-ghost badge-sm">Desktop Support Technician</span>
+				  <span class="badge badge-ghost badge-sm">{product.brand}</span>
 				</td>
-				<td>Purple</td>
+				<td><div class="rating">
+					<input type="radio" name="rating-4" class="mask mask-star-2 bg-green-500" />
+					<input type="radio" name="rating-4" class="mask mask-star-2 bg-green-500" checked />
+					<input type="radio" name="rating-4" class="mask mask-star-2 bg-green-500" />
+					<input type="radio" name="rating-4" class="mask mask-star-2 bg-green-500" />
+					<input type="radio" name="rating-4" class="mask mask-star-2 bg-green-500" />
+				  </div></td>
 				<th>
 				  <button class="btn btn-ghost btn-xs">details</button>
 				</th>
 			  </tr>
 	 {/each} 
-			<tr>
-			  <th>
-				<label>
-				  <input type="checkbox" class="checkbox" />
-				</label>
-			  </th>
-			  <td>
-				<div class="flex items-center gap-3">
-				  <div class="avatar">
-					<div class="mask mask-squircle w-12 h-12">
-					  <img src="/tailwind-css-component-profile-2@56w.png" alt="Avatar Tailwind CSS Component" />
-					</div>
-				  </div>
-				  <div>
-					<div class="font-bold">Hart Hagerty</div>
-					<div class="text-sm opacity-50">United States</div>
-				  </div>
-				</div>
-			  </td>
-			  <td>
-				Zemlak, Daniel and Leannon
-				<br/>
-				<span class="badge badge-ghost badge-sm">Desktop Support Technician</span>
-			  </td>
-			  <td>Purple</td>
-			  <th>
-				<button class="btn btn-ghost btn-xs">details</button>
-			  </th>
-			</tr>
-			
+		
 		  </tbody>
 	
 		  <tfoot>
@@ -136,4 +120,5 @@
 		  </tfoot>
 		  
 		</table>
-	  </div> -->
+	  </div>
+	
